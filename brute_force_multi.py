7d320583f04d47d4e407505d9d2c2b6d49a778cd
Vítor Queijo <51131016+vitorqueijo@users.noreply.github.com>
2020-04-10 23:47:27 +0000

    adding simple input method and thread counting

--- brute_force_multi.py
+++ brute_force_multi.py
@@ -3,8 +3,9 @@ import gnupg
 
 
 def divide_list(wordlist, num_to_split):
-	if num_to_split > 4:
-		print("Changing to 4 as a default")
+	if num_to_split > mp.cpu_count():
+		print("Changing to {} as a default".format(mp.cpu_count()))
+		num_to_split = mp.cpu_count()
 	num_to_split = 4
 	splitted_wordlist = None
 	try:
@@ -15,7 +16,7 @@ def divide_list(wordlist, num_to_split):
 					if splitted_wordlist:
 						splitted_wordlist.close()
 					n_splitted = "rockyou_{}.txt".format(n_line+num_to_split)
-					splitted_wordlist = open(n_splitted, "w")
+					splitted_wordlist = open(n_splitted, "wb")
 				splitted_wordlist.write(str(line))
 			if splitted_wordlist:
 				splitted_wordlist.close()
@@ -32,3 +33,18 @@ def decrypt_gpg(gpg_file):
 			print('status: ', decrypted_file.status)
 			print('stderr: ', decrypted_file.stderr)
 
+def brute_force_simple(wordlist, gpg_file):
+	with open(wordlist, 'rb') as words:
+		pwd_list = words.read().splitlines()
+		for pwd in pwd_list:
+			try:
+				decrypt_gpg(gpg_file)
+			except Exception as e:
+				print("Exception: ", e)
+
+if __name__ == "__main__":
+	
+	print("There're {} cores available on machine".format(mp.cpu_count())
+	cpu_cores = input(int("How many cores do you want to use?"))
+	if cpu_cores > mp.cpu_count():
+		print("setting up to maximum")
