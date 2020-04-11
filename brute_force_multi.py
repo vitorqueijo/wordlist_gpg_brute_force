97dbd6884d92d0960eed6cbf2a661254a6fa3f75
Vítor Queijo <51131016+vitorqueijo@users.noreply.github.com>
2020-04-11 16:23:21 +0000

    Update brute_force_multi.py

--- brute_force_multi.py
+++ brute_force_multi.py
@@ -7,23 +7,23 @@ import multiprocessing as mp
 import gnupg
 
 
-def worker(input, output):
-    for func, args in iter(input.get, 'STOP'):
-        result = calculate(func, args)
-	print(result)
-        sema.release()
-
-def calculate(func, args):
-    result = func(*args)
-    return '%s says that %s%s = %s' % \
-        (mp.current_process().name, func.__name__, args, result)
+#def worker(input, output):
+#	for func, args in iter(input.get, 'STOP'):
+#		result = calculate(func, args)
+#		print(result)
+#	sema.release()
+#
+#def calculate(func, args):
+#    result = func(*args)
+#    return '%s says that %s%s = %s' % \
+#       (mp.current_process().name, func.__name__, args, result)
 
 def chunkfy_list(wordlist):
 	try:
 		with open(wordlist, 'rb') as words:
 			wd = words.read().splitlines()
 			size_wd = len(wd)
-			chunks = [wd[x:x+10000] for line in range(0, size_wd, 10000)]
+			chunks = [wd[line:line+10000] for line in range(0, size_wd, 10000)]
 
 	except OverflowError as error:
 		print("OverFlow: ", error)
@@ -31,8 +31,10 @@ def chunkfy_list(wordlist):
 		print("Exception in divided: ", exception)
 	return chunks
 
-def brute_force_simple(wordlist):
-	target = '' # edit to your GPG file
+def brute_force_simple(name, wordlist, sema):
+	sema.acquire()
+	print("Process {} running".format(name))
+	target = 'lab1.gpg' # edit to your GPG file
 	gpg = gnupg.GPG()
 	time_count = time.time()
 	for pwd in wordlist:
@@ -42,9 +44,11 @@ def brute_force_simple(wordlist):
 				print('status: ', decrypted_file.status)
 				print('stderr: ', decrypted_file.stderr)
 				print("PASSWORD: ", pwd)
+				sema.release()
 		except Exception as e:
 			print("Exception: ", e)
 	print("not yet {}".format(time.time() - time_count))
+	sema.release()
 
 if __name__ == "__main__":
 	# Setting up numbers of cores to use
@@ -56,28 +60,27 @@ if __name__ == "__main__":
 	#TODO: argparse for any wordlist and gpg file
 	# Not a good way if you don't want to freeze your computer
 	# wordlists = [wd for wd in os.listdir('.') if os.path.isfile(wd) if wd.endswith("_*.txt")]
+	print("Attack Initiated!")	
 	start = time.time()
-	wordlists = chunkfy_list('') # edit to your wordlist
+	resourses = chunkfy_list('rockyou.txt') # edit to your wordlist
 	print("wordlist chunkfied, elapsed time: ", time.time() - start)
 	print("Setting up concurrency at 1% of all tasks")
-	concurrency = len(TASKS) * 0.01
-	num_tasks = len(TASKS)
+	print("Setting up tasks")
+	concurrency = int(len(resourses) * 0.01)
+	num_tasks = len(resourses)
 	sema = Semaphore(concurrency)
-
-	# Starting workers
-	print("Setting up tasks, elapsed time: ", time.time() - start)
-	TASKS = [(brute_force_simple, wd) for wd in wordlists]
 
 	# Start
 	processes = list()
-	for i in range(num_tasks):
-		sema.acquired()
-		process = mp.Process(target=worker, args=(i, sema)).start()
+	for i, r in enumerate(resourses):
+		process = mp.Process(target=brute_force_simple, args=(i, r, sema))
 		processes.append(process)
 		process.start()
+
 	# Finish
 	for p in processes:
 		p.join()
+
 	print("Elapsed time: ", time.time() - start)
 	end = time.time() - start
 	print("End of process, time elapsed: {}".format(end))
