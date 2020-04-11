f8047cb92872a81e595e6fc6dddf09b9145479d9
Vítor Queijo <51131016+vitorqueijo@users.noreply.github.com>
2020-04-11 16:06:40 +0000

    Update brute_force_multi.py

--- brute_force_multi.py
+++ brute_force_multi.py
@@ -10,7 +10,8 @@ import gnupg
 def worker(input, output):
     for func, args in iter(input.get, 'STOP'):
         result = calculate(func, args)
-        output.put(result)
+	print(result)
+        sema.release()
 
 def calculate(func, args):
     result = func(*args)
@@ -31,7 +32,7 @@ def chunkfy_list(wordlist):
 	return chunks
 
 def brute_force_simple(wordlist):
-	target = 'lab1.gpg' # edit to your GPG file
+	target = '' # edit to your GPG file
 	gpg = gnupg.GPG()
 	time_count = time.time()
 	for pwd in wordlist:
@@ -40,11 +41,9 @@ def brute_force_simple(wordlist):
 			if decrypted_file.ok:
 				print('status: ', decrypted_file.status)
 				print('stderr: ', decrypted_file.stderr)
-				sema.release()
 				print("PASSWORD: ", pwd)
 		except Exception as e:
 			print("Exception: ", e)
-	sema.release()
 	print("not yet {}".format(time.time() - time_count))
 
 if __name__ == "__main__":
@@ -54,12 +53,11 @@ if __name__ == "__main__":
 	#if cpu_cores > mp.cpu_count() and cpu_cores <= 0:
 	#	print("setting up to maximum")
 	#	cpu_cores = mp.cpu_count()
-	NUM_PROCESSES = cpu_cores * 2
 	#TODO: argparse for any wordlist and gpg file
 	# Not a good way if you don't want to freeze your computer
 	# wordlists = [wd for wd in os.listdir('.') if os.path.isfile(wd) if wd.endswith("_*.txt")]
 	start = time.time()
-	wordlists = chunkfy_list('rockyou.txt') # edit to your wordlist
+	wordlists = chunkfy_list('') # edit to your wordlist
 	print("wordlist chunkfied, elapsed time: ", time.time() - start)
 	print("Setting up concurrency at 1% of all tasks")
 	concurrency = len(TASKS) * 0.01
@@ -70,21 +68,16 @@ if __name__ == "__main__":
 	print("Setting up tasks, elapsed time: ", time.time() - start)
 	TASKS = [(brute_force_simple, wd) for wd in wordlists]
 
-	# Submitting
-	for task in TASKS:
-		q.put(task)
-
 	# Start
-	for process in range(NUM_PROCESSES):
-		mp.Process(target=worker, args=(q, done)).start()
-	# Results
-	print('Results: \n')
-	for i in range(len(TASKS)):
-	        print('\n', done.get())
-	# Then, stop done
-	for i in range(NUM_PROCESSES):
-        	q.put('STOP')
-
+	processes = list()
+	for i in range(num_tasks):
+		sema.acquired()
+		process = mp.Process(target=worker, args=(i, sema)).start()
+		processes.append(process)
+		process.start()
+	# Finish
+	for p in processes:
+		p.join()
 	print("Elapsed time: ", time.time() - start)
 	end = time.time() - start
 	print("End of process, time elapsed: {}".format(end))
