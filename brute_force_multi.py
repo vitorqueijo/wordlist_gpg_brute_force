bd2a93a683bdb6e50f993b8156d12c1aab601e3c
Vítor Queijo <51131016+vitorqueijo@users.noreply.github.com>
2020-04-11 15:58:22 +0000

    Semaphore strategy added

--- brute_force_multi.py
+++ brute_force_multi.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 
+from multiprocessing import Semaphore
 import multiprocessing as mp
 import gnupg
 
@@ -16,29 +17,21 @@ def calculate(func, args):
     return '%s says that %s%s = %s' % \
         (mp.current_process().name, func.__name__, args, result)
 
-def divide_list(wordlist, num_to_split):
-	if num_to_split > mp.cpu_count():
-		print("Changing to {} as a default".format(mp.cpu_count()))
-		num_to_split = mp.cpu_count()
-	splitted_wordlists = list()
-	n_wordlist = list()
+def chunkfy_list(wordlist):
 	try:
 		with open(wordlist, 'rb') as words:
 			wd = words.read().splitlines()
-			for n_line, line in enumerate(wd):
-				n_wordlist.append(line)
-				if n_line % num_to_split == 0:
-					splitted_wordlists.append([splitted_wordlists])
-					n_wordlist = list()
+			size_wd = len(wd)
+			chunks = [wd[x:x+10000] for line in range(0, size_wd, 10000)]
 
 	except OverflowError as error:
 		print("OverFlow: ", error)
 	except Exception as exception:
 		print("Exception in divided: ", exception)
-	return splitted_wordlists
+	return chunks
 
 def brute_force_simple(wordlist):
-	target = '' # edit to your GPG file
+	target = 'lab1.gpg' # edit to your GPG file
 	gpg = gnupg.GPG()
 	time_count = time.time()
 	for pwd in wordlist:
@@ -47,33 +40,35 @@ def brute_force_simple(wordlist):
 			if decrypted_file.ok:
 				print('status: ', decrypted_file.status)
 				print('stderr: ', decrypted_file.stderr)
-				return pwd
+				sema.release()
+				print("PASSWORD: ", pwd)
 		except Exception as e:
 			print("Exception: ", e)
-	return "not yet {}".format(time.time() - time_count)
+	sema.release()
+	print("not yet {}".format(time.time() - time_count))
 
 if __name__ == "__main__":
 	# Setting up numbers of cores to use
-	print("There're {} cores available on machine".format(mp.cpu_count()))
-	cpu_cores = int(input("How many cores do you want to use?\n"))
-	if cpu_cores > mp.cpu_count() and cpu_cores <= 0:
-		print("setting up to maximum")
-		cpu_cores = mp.cpu_count()
+	#print("There're {} cores available on machine".format(mp.cpu_count()))
+	#cpu_cores = int(input("How many cores do you want to use?\n"))
+	#if cpu_cores > mp.cpu_count() and cpu_cores <= 0:
+	#	print("setting up to maximum")
+	#	cpu_cores = mp.cpu_count()
 	NUM_PROCESSES = cpu_cores * 2
 	#TODO: argparse for any wordlist and gpg file
 	# Not a good way if you don't want to freeze your computer
 	# wordlists = [wd for wd in os.listdir('.') if os.path.isfile(wd) if wd.endswith("_*.txt")]
-	wordlists = divide_list('rockyou.txt', NUM_PROCESSES) # edit to your wordlist
-	gpg_file = open(target, 'rb')
 	start = time.time()
+	wordlists = chunkfy_list('rockyou.txt') # edit to your wordlist
+	print("wordlist chunkfied, elapsed time: ", time.time() - start)
+	print("Setting up concurrency at 1% of all tasks")
+	concurrency = len(TASKS) * 0.01
+	num_tasks = len(TASKS)
+	sema = Semaphore(concurrency)
 
 	# Starting workers
-	print("Starting workers", time.time() - start)
-	TASKS = [(brute_force_simple, w) for wd in wordlists]
-
-	# Setting queues
-	q = mp.Queue()
-	done = mp.Queue()
+	print("Setting up tasks, elapsed time: ", time.time() - start)
+	TASKS = [(brute_force_simple, wd) for wd in wordlists]
 
 	# Submitting
 	for task in TASKS:
