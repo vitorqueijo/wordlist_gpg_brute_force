cfe3bc388efc352bb69617b696fc8a50c76bac62
Vítor Queijo <51131016+vitorqueijo@users.noreply.github.com>
2020-04-11 03:02:03 -0300

    chuncking enabled

--- brute_force_multi.py
+++ brute_force_multi.py
@@ -54,21 +54,26 @@ def brute_force_simple(wordlist, gpg_file, time_count):
 if __name__ == "__main__":
 	# Setting up numbers of cores to use
 	print("There're {} cores available on machine".format(mp.cpu_count()))
-	cpu_cores = int(input("How many cores do you want to use?"))
+	cpu_cores = int(input("How many cores do you want to use?\n"))
 	if cpu_cores > mp.cpu_count() and cpu_cores <= 0:
 		print("setting up to maximum")
 		cpu_cores = mp.cpu_count()
 	#TODO: argparse for any wordlist and gpg file
 	# Not a good way if you don't want to freeze your computer
 	# wordlists = [wd for wd in os.listdir('.') if os.path.isfile(wd) if wd.endswith("_*.txt")]
-	wordlists = divide_list('', cpu_cores) # edit to your wordlist
+	wordlists = divide_list('rockyou.txt', cpu_cores) # edit to your wordlist
 	gpg_file = open(target, 'rb')
 	start = time.time()
-	# Source: https://stackoverflow.com/questions/5442910/python-multiprocessing-pool-map-for-multiple-arguments
+
 	# Starting workers
 	print("Starting workers", time.time() - start)
-	NUM_PROCESSES = cpu_cores
-	TASKS = [(brute_force_simple, (wordlist, gpg_file, start)) for wordlist in list_wordlist]
+	NUM_PROCESSES = cpu_cores * 2
+	n = (len(wordlists)/(len(wordlists)**2))
+	for wd in wordlists:
+		wordlists_c = [wd[i * n:(i + 1) * n] \
+						for i in range((len(my_list) + n - 1) // n )]
+		TASKS = [(brute_force_simple, (wordlist, gpg_file, start)) \
+					for wordlist in wordlists_c]
 
 	# Setting queues
 	q = mp.Queue()
